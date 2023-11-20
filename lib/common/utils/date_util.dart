5935bc78367debc3a2b52036b8e21ee7403106a3
SeongGwan <tjdrhks0808@naver.com>
2023-11-20 21:13:07 +0900

    [Feature] 변경 신청 확인 화면 CX 수정

--- lib/common/utils/date_util.dart
+++ lib/common/utils/date_util.dart
@@ -4,4 +4,18 @@ class DateUtil {
   static List<int> lastDayOfMonths(int year) {
     return year % 4 == 0 ? leapYear : notLeapYear;
   }
+
+  static String getKoreanWeekDay(DateTime day) {
+    List<String> weekDays = ['일', '월', '화', '수', '목', '금', '토'];
+    return weekDays[day.weekday];
+  }
+
+  static String getKoreanDayAndHour(DateTime day) {
+    String hour = day.hour < 10 ? '0${day.hour}' : day.hour.toString();
+    return '${day.month}월 ${day.day}일 ${getKoreanWeekDay(day)}요일 $hour:00';
+  }
+
+  static String getKoreanDayAndExactHour(DateTime day, String hour) {
+    return '${day.month}월 ${day.day}일 ${getKoreanWeekDay(day)}요일 $hour';
+  }
 }
--- lib/user_timetable/proposal.dart
+++ lib/user_timetable/proposal.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gymming_app/common/colors.dart';
 import 'package:gymming_app/user_timetable/user_timetable.dart';
+
+import '../common/utils/date_util.dart';
 
 class Proposal extends StatelessWidget {
   final String type;
@@ -19,139 +22,179 @@ class Proposal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                '운동 일정 $type을(를)\n신청하셨습니다.',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                '일정 $type이(가) 확정된 것이 아니며, 트레이너 및\n지점 확인 후 승인 여부를 알려드리겠습니다.\n(최대 3시간 소요)',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16.0,
-                ),
-              ),
-              SizedBox(height: 60),
-              Column(
-                // mainAxisAlignment: MainAxisAlignment.start,
-                // crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Column(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+            child: Column(children: [
+              Expanded(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        children: [
-                          Icon(Icons.calendar_today,
-                              color: Colors.white, size: 30.0),
-                          Text("변경 전",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16.0,
-                              ))
-                        ],
-                      ),
-                      SizedBox(height: 20),
+                    children: [
                       Text(
-                        '${originDay.month}월${originDay.day}일 ${originDay.hour}:00',
+                        '운동 일정 $type을(를)\n신청하셨습니다.',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      Text(
+                        '일정 $type이(가) 확정된 것이 아니며, 트레이너 및\n지점 확인 후 승인 여부를 알려드리겠습니다.\n(최대 3시간 소요)',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 16.0,
                         ),
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        children: [
-                          Icon(Icons.calendar_today,
-                              color: Colors.white, size: 30.0),
-                          Text("변경 후",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16.0,
-                              ))
-                        ],
-                      ),
-                 
-                      SizedBox(height: 20),
-                      Text(
-                        '${selectedDay.month}월${selectedDay.day}일 $selectedTime',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        children: [
-                          Icon(Icons.calendar_today,
-                              color: Colors.white, size: 30.0),
-                          Text("$type 사유",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16.0,
-                              ))
-                        ],
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        reason,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
+                      SizedBox(height: 40),
+                      buildTitleAndTime('변경 전', originDay, '', SECONDARY_COLOR),
+                      SizedBox(height: 40),
+                      buildTitleAndTime(
+                          '변경 후', selectedDay, selectedTime, Colors.white),
+                      SizedBox(height: 40),
+                      buildTitleAndReason('변경 사유', reason),
+                    ]),
               ),
-              SizedBox(height: 40),
-              SizedBox(height: 40),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('변경 취소'),
-                    style: ElevatedButton.styleFrom(primary: Colors.grey),
+                children: [
+                  SizedBox(
+                    width: 169,
+                    height: 56,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => UserTimeTable()),
+                          (Route<dynamic> route) =>
+                              false, // 모든 라우트를 제거하므로 false를 반환합니다.
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(BTN_COLOR),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                        ),
+                      ),
+                      child: Text(
+                        '변경 취소',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => UserTimeTable()),
-                            (Route<dynamic> route) => false, // 모든 라우트를 제거하므로 false를 반환합니다.
-                      );
-                    },
-                    child: Text('확인'),
-                    style: ElevatedButton.styleFrom(primary: Colors.blue),
+                  SizedBox(
+                    width: 169,
+                    height: 56,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => UserTimeTable()),
+                          (Route<dynamic> route) =>
+                              false, // 모든 라우트를 제거하므로 false를 반환합니다.
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(BTN_COLOR),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                        ),
+                      ),
+                      child: Text(
+                        '확인',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: PRIMARY_COLOR,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ],
+            ]),
           ),
+        ));
+  }
+
+  Widget buildTitleAndTime(
+      String title, DateTime time, String hour, Color color) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(
+          'assets/clock.png',
+          width: 20,
+          height: 20,
         ),
-      ),
+        SizedBox(width: 12),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(title,
+                style: TextStyle(
+                  color: color,
+                  fontSize: 18.0,
+                )),
+            SizedBox(height: 8),
+            Text(
+              hour.isEmpty
+                  ? DateUtil.getKoreanDayAndHour(time)
+                  : DateUtil.getKoreanDayAndExactHour(time, hour),
+              style: TextStyle(
+                color: color,
+                fontSize: 20.0,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget buildTitleAndReason(String title, String reason) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(
+          'assets/talkBox.png',
+          width: 20,
+          height: 20,
+        ),
+        SizedBox(width: 12),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(title,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                )),
+            SizedBox(height: 8),
+            Text(
+              reason,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
