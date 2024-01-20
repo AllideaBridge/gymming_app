18ba79c12b7c76a85a2e5dae0ba816012e89b744
choseongmin <sm1110.cho@samsung.com>
2024-01-20 16:59:46 +0900

    [Request_Detail] schedule_clicked 오류 수정

--- lib/modal/schedule_clicked.dart
+++ lib/modal/schedule_clicked.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gymming_app/common/colors.dart';
+import 'package:gymming_app/common/component/icon_label.dart';
 import 'package:gymming_app/common/utils/date_util.dart';
 import 'package:gymming_app/user_timetable/model/schedule_info.dart';
 import 'package:provider/provider.dart';
@@ -21,119 +22,88 @@ class ScheduleClicked extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          clickedScheduleItem(Icons.alarm, "일시",
-              DateUtil.getKoreanDayAndHour(scheduleInfo.startTime)),
-          clickedScheduleItem(
-            Icons.calendar_today_outlined,
-            "일정",
-            '${scheduleInfo.lessonName} | ${scheduleInfo.trainerName} 트레이너',
+          IconLabel(
+            iconData: Icons.alarm,
+            title: "일시",
+            content: DateUtil.getKoreanDayAndHour(scheduleInfo.startTime),
+            titleColor: SECONDARY_COLOR,
+            contentColor: Colors.white,
           ),
-          clickedScheduleItem(Icons.location_on, "장소",
-              '${scheduleInfo.centerName} | ${scheduleInfo.centerLocation}'),
+          IconLabel(
+            iconData: Icons.calendar_today_outlined,
+            title: "일정",
+            content:
+            '${scheduleInfo.lessonName} | ${scheduleInfo.trainerName} 트레이너',
+            titleColor: SECONDARY_COLOR,
+            contentColor: Colors.white,
+          ),
+          IconLabel(
+            iconData: Icons.location_on,
+            title: "장소",
+            content:
+            '${scheduleInfo.centerName} | ${scheduleInfo.centerLocation}',
+            titleColor: SECONDARY_COLOR,
+            contentColor: Colors.white,
+          ),
           SizedBox(
               height: 56,
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  secondaryButton(
-                      "취소하기",
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ScheduleCancelConfirm(
-                              scheduleInfo: scheduleInfo))),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: BTN_COLOR,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6),
+                          )),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(
+                            builder: (context) =>
+                                ScheduleCancelConfirm(
+                                    scheduleInfo: scheduleInfo)));
+                      },
+                      child: Text(
+                        "취소하기",
+                        style: TextStyle(
+                          color: PRIMARY_COLOR,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
                   SizedBox(width: 12),
-                  primaryButton(
-                      "변경하기",
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ScheduleChange(
-                                originDay: Provider.of<StateDateTime>(context)
-                                    .selectedDateTime,
-                                scheduleInfo: scheduleInfo,
-                              ))),
+                  Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: PRIMARY_COLOR,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(6),
+                            )),
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(
+                              builder: (context) =>
+                                  ScheduleChange(
+                                    originDay: Provider
+                                        .of<StateDateTime>(context)
+                                        .selectedDateTime,
+                                    scheduleInfo: scheduleInfo,
+                                  )));
+                        },
+                        child: Text(
+                          "변경하기",
+                          style: TextStyle(
+                            color: INDICATOR_COLOR,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ))
                 ],
               ))
         ],
       ),
     );
-  }
-
-  Widget clickedScheduleItem(IconData iconData, String title, String content) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Icon(
-          iconData,
-          color: SECONDARY_COLOR,
-          size: 20,
-        ),
-        SizedBox(
-          width: 12,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: TextStyle(color: SECONDARY_COLOR, fontSize: 18),
-            ),
-            Text(
-              content,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500),
-            )
-          ],
-        )
-      ],
-    );
-  }
-
-  Widget secondaryButton(
-      String title, BuildContext context, MaterialPageRoute route) {
-    return Expanded(
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-            backgroundColor: BTN_COLOR,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(6),
-            )),
-        onPressed: () {
-          Navigator.push(context, route);
-        },
-        child: Text(
-          title,
-          style: TextStyle(
-            color: PRIMARY_COLOR,
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget primaryButton(
-      String title, BuildContext context, MaterialPageRoute route) {
-    return Expanded(
-        child: ElevatedButton(
-      style: ElevatedButton.styleFrom(
-          backgroundColor: PRIMARY_COLOR,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(6),
-          )),
-      onPressed: () {
-        Navigator.push(context, route);
-      },
-      child: Text(
-        title,
-        style: TextStyle(
-          color: INDICATOR_COLOR,
-          fontSize: 20,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-    ));
   }
 }
