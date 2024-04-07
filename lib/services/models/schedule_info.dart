73f6076edeaa91924250ae52f08ec1e866e41a48
choseongmin <sm1110.cho@samsung.com>
2024-04-07 14:26:14 +0900

    [schedule-info] 불필요 endtime 필드 제거

--- lib/services/models/schedule_info.dart
+++ lib/services/models/schedule_info.dart
@@ -1,7 +1,6 @@
 class ScheduleInfo {
   final int _scheduleId;
   final DateTime _startTime;
-  final DateTime _endTime;
   final String _lessonName;
   final String _trainerName;
   final String _centerName;
@@ -9,7 +8,7 @@ class ScheduleInfo {
   final int _remainDays;
 
 
-  ScheduleInfo(this._scheduleId, this._startTime, this._endTime, this._lessonName,
+  ScheduleInfo(this._scheduleId, this._startTime, this._lessonName,
       this._trainerName, this._centerName, this._centerLocation, this._remainDays);
 
   int get scheduleId => _scheduleId;
@@ -17,14 +16,12 @@ class ScheduleInfo {
   String get centerName => _centerName;
   String get trainerName => _trainerName;
   String get lessonName => _lessonName;
-  DateTime get endTime => _endTime;
   DateTime get startTime => _startTime;
   int get remainDays => _remainDays;
 
   factory ScheduleInfo.fromJson(Map<String, dynamic> json) {
     return ScheduleInfo(
         json["schedule_id"],
-        DateTime.parse(json["schedule_start_time"]),
         DateTime.parse(json["schedule_start_time"]),
         //todo endTime
         json["lesson_name"],
