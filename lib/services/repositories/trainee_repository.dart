a7f67f62d06c1ff7a43e7385ab790927d1efe4ad
choseongmin <sm1110.cho@samsung.com>
2024-05-31 19:58:28 +0900

    dummy api 값 수정

--- lib/services/repositories/trainee_repository.dart
+++ lib/services/repositories/trainee_repository.dart
@@ -1,64 +1,6 @@
 import '../models/trainee_detail.dart';
-import '../models/trainee_list.dart';
 
 class TraineeRepository {
-  final List<TraineeList> _dummyTrainees = [
-    TraineeList(
-      name: '김헬스',
-      profileImg: 'assets/images/trainerExample.png',
-      weekday: '월, 수, 금',
-      usedDay: 3,
-      totalDay: 10,
-      registeredDay: '23.10.20',
-      lastDay: '23.10.20',
-    ),
-    TraineeList(
-      name: '조헬스',
-      profileImg: 'assets/images/trainerExample2.png',
-      weekday: '월, 수',
-      usedDay: 6,
-      totalDay: 10,
-      registeredDay: '23.11.10',
-      lastDay: '23.11.10',
-    ),
-    TraineeList(
-      name: '이헬스',
-      profileImg: 'assets/images/trainerExample3.png',
-      weekday: '금',
-      usedDay: 0,
-      totalDay: 10,
-      registeredDay: '23.04.08',
-      lastDay: '23.04.08',
-    ),
-    TraineeList(
-      name: '박헬스',
-      profileImg: 'assets/images/trainerExample.png',
-      weekday: '월, 수, 금',
-      usedDay: 3,
-      totalDay: 10,
-      registeredDay: '23.10.20',
-      lastDay: '23.10.20',
-    ),
-    TraineeList(
-      name: '임헬스',
-      profileImg: 'assets/images/trainerExample2.png',
-      weekday: '월, 수',
-      usedDay: 6,
-      totalDay: 10,
-      registeredDay: '23.11.10',
-      lastDay: '23.11.10',
-    ),
-    TraineeList(
-      name: '최헬스',
-      profileImg: 'assets/images/trainerExample3.png',
-      weekday: '금',
-      usedDay: 0,
-      totalDay: 10,
-      registeredDay: '23.04.08',
-      lastDay: '23.04.08',
-    ),
-  ];
-
   final TraineeDetail _dummyTrainee = TraineeDetail(
       profileImg: 'assets/images/trainerExample3.png',
       name: '김헬스',
@@ -81,10 +23,6 @@ class TraineeRepository {
         DateTime(2024, 2, 22),
       ],
       specificDetail: "체중 증량이 목표");
-
-  List<TraineeList> getTraineeList() {
-    return _dummyTrainees;
-  }
 
   TraineeDetail getTraineeDetail() {
     return _dummyTrainee;
