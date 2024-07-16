05b523140b63cef09b56c702fecf0708285cfc1b
SeongGwan <tjdrhks0808@naver.com>
2024-07-16 20:47:47 +0900

    [Feature] 트레이너 회원가입 Step1 구현

--- lib/components/layouts/gympro_infos/gympro_info_step1.dart
+++ lib/components/layouts/gympro_infos/gympro_info_step1.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gymming_app/common/colors.dart';
+import 'package:gymming_app/components/birthday_select.dart';
+import 'package:gymming_app/components/input_filed.dart';
+import 'package:gymming_app/components/phone_number_select.dart';
+import 'package:gymming_app/components/profile_image.dart';
 
 class GymproInfoStep1 extends StatefulWidget {
   const GymproInfoStep1({super.key});
@@ -8,17 +13,191 @@ class GymproInfoStep1 extends StatefulWidget {
 }
 
 class GymproInfoStep1State extends State<GymproInfoStep1> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _historyController = TextEditingController();
+  final Map<String, dynamic> _model = {
+    'name': '',
+    'phoneNumber': '',
+    'birth': DateTime.now(),
+    'gender': 'M',
+    'history': '',
+  };
+  final Map<String, bool> _validate = {
+    'name': false,
+    'phoneNumber': false,
+    'birth': false,
+  };
+  int _historyLength = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _historyController.addListener(() {
+      setState(() {
+        _model['history'] = _historyController.text;
+        _historyLength = _historyController.text.length;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _historyController.dispose();
+    super.dispose();
+  }
+
+  void onChangedName(bool isValid) {
+    setState(() {
+      _model['name'] = _nameController.text;
+      _validate['name'] = isValid;
+    });
+  }
+
+  void onChangedBirthday(DateTime birthDay) {
+    setState(() {
+      _model['birth'] = birthDay;
+      _validate['birth'] = true;
+    });
+  }
+
+  void onChangedPhoneNumber(String phoneNumber) {
+    if (phoneNumber.length == 11) {
+      setState(() {
+        _model['phoneNumber'] = phoneNumber;
+        _validate['phoneNumber'] = true;
+      });
+    } else {
+      setState(() {
+        _model['phoneNumber'] = '';
+        _validate['phoneNumber'] = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Column(
         children: [
-          Container(height: 300.0, color: Colors.white),
-          SizedBox(height: 12.0),
-          Container(height: 300.0, color: Colors.white),
-          SizedBox(height: 12.0),
-          Container(height: 300.0, color: Colors.white),
+          ProfileImage(
+            size: 130.0,
+          ),
+          SizedBox(height: 60.0),
+          InputField(
+            controller: _nameController,
+            title: '이름',
+            validator: (val) {
+              if (val.length < 1) return '이름은 필수값입니다.';
+            },
+            onValidationChanged: onChangedName,
+            isRequired: true,
+          ),
+          SizedBox(height: 60.0),
+          PhoneNumberSelect(
+            title: '전화번호',
+            setter: onChangedPhoneNumber,
+          ),
+          SizedBox(height: 60.0),
+          BirthdaySelect(
+            setter: onChangedBirthday,
+          ),
+          SizedBox(height: 60.0),
+          _buildGenderSelect(),
+          SizedBox(height: 60.0),
+          _buildHistoryField(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGenderSelect() {
+    bool isSelectedMale = true;
+
+    if (_model['gender'] == 'M') {
+      isSelectedMale = true;
+    } else {
+      isSelectedMale = false;
+    }
+
+    return Row(
+      children: [
+        Expanded(
+            child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: isSelectedMale ? PRIMARY_COLOR : BTN_COLOR,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6)),
+              minimumSize: Size(160, 56)),
+          onPressed: () {
+            setState(() {
+              _model['gender'] = 'M';
+            });
+          },
+          child: Text(
+            '남',
+            style: TextStyle(
+              color: isSelectedMale ? INDICATOR_COLOR : Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        )),
+        SizedBox(width: 8.0),
+        Expanded(
+            child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: isSelectedMale ? BTN_COLOR : PRIMARY_COLOR,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6)),
+              minimumSize: Size(160, 56)),
+          onPressed: () {
+            setState(() {
+              _model['gender'] = 'F';
+            });
+          },
+          child: Text(
+            '여',
+            style: TextStyle(
+              color: isSelectedMale ? Colors.white : INDICATOR_COLOR,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        )),
+      ],
+    );
+  }
+
+  Widget _buildHistoryField() {
+    return SizedBox(
+      height: 180,
+      child: Stack(
+        children: [
+          TextField(
+            controller: _historyController,
+            maxLines: 50,
+            decoration: InputDecoration(
+              hintText: '트레이너 이력을 입력해주세요.',
+              hintStyle: TextStyle(fontSize: 18, color: TERITARY_COLOR),
+              filled: true,
+              fillColor: BACKGROUND_COLOR,
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+            ),
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
+          Positioned(
+            right: 16,
+            bottom: 12,
+            child: Text(
+              '$_historyLength / 255',
+              style: TextStyle(fontSize: 12, color: Colors.white),
+            ),
+          ),
         ],
       ),
     );
