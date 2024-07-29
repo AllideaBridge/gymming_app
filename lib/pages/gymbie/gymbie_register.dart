e106d21843218fdca1f5cec76079e40341747057
choseongmin <sm1110.cho@samsung.com>
2024-07-29 18:29:12 +0900

    회원 회원가입 이름 벨리데이션 변경

--- lib/pages/gymbie/gymbie_register.dart
+++ lib/pages/gymbie/gymbie_register.dart
@@ -56,6 +56,9 @@ class _GymbieRegisterState extends State<GymbieRegister> {
                             controller: _nameController,
                             title: "이름",
                             isRequired: true,
+                            validator: (val) {
+                              if (val.length < 1) return '이름은 필수값입니다.';
+                            },
                             onValidationChanged: (value) {
                               setState(() {
                                 _isNameValidate = value;
