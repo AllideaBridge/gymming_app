b94318e6f88cfcdb04c562e103451f9cfb1208ae
SeongGwan <tjdrhks0808@naver.com>
2024-08-20 20:04:24 +0900

    [Feature] 트레이너 홈 화면 스케줄 선택 시 디테일 표시 추가

--- lib/pages/gympro/gympro_home/lesson_data_source.dart
+++ lib/pages/gympro/gympro_home/lesson_data_source.dart
@@ -1,35 +1,19 @@
-import 'package:flutter/material.dart';
 import 'package:gymming_app/common/colors.dart';
 import 'package:gymming_app/services/models/lesson_list.dart';
-import 'package:gymming_app/services/models/meeting.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class LessonDataSource extends CalendarDataSource {
   LessonDataSource(List<LessonList> source) {
-    List<Meeting> meetings = [];
+    List<Appointment> _appointments = <Appointment>[];
     for (LessonList lessonList in source) {
-      meetings.add(Meeting.fromLessonList(lessonList, PRIMARY_COLOR));
+      Appointment appointment = Appointment(
+          startTime: lessonList.startTime,
+          endTime: lessonList.startTime
+              .add(Duration(minutes: lessonList.lessonMinute)),
+          subject: lessonList.userName,
+          color: PRIMARY_COLOR);
+      _appointments.add(appointment);
     }
-    appointments = meetings;
-  }
-
-  @override
-  DateTime getStartTime(int index) {
-    return appointments![index].from;
-  }
-
-  @override
-  DateTime getEndTime(int index) {
-    return appointments![index].to;
-  }
-
-  @override
-  String getSubject(int index) {
-    return appointments![index].name;
-  }
-
-  @override
-  Color getColor(int index) {
-    return appointments![index].background;
+    appointments = _appointments;
   }
 }
